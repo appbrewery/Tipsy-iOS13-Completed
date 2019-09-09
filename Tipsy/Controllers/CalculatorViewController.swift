3871f8bd4fb9bdf0458d35a2533b0495d66e64a2
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-09 18:07:26 +0100

    Step 5 Solution

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -17,39 +17,36 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     var tip = 0.10
+    var numberOfPeople = 2
     
     @IBAction func tipChanged(_ sender: UIButton) {
-        //Deselect all tip buttons via IBOutlets
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
-        
-        //Make the button that triggered the IBAction selected.
         sender.isSelected = true
         
-        //Get the current title of the button that was pressed.
         let buttonTitle = sender.currentTitle!
-        
-        //Remove the last character (%) from the title then turn it back into a String.
         let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
-        
-        //Turn the String into a Double.
         let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
-        
-        //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
         tip = buttonTitleAsANumber / 100
 
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        //Get the stepper value using sender.value, round it down to a whole number then set it as the text in
+        //the splitNumberLabel
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
         
+        //Set the numberOfPeople property as the value of the stepper as a whole number.
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(tip)
+        print(numberOfPeople)
     }
     
 }
+
 
 
 
