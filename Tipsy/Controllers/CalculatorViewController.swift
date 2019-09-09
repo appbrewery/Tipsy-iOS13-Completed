2275a9711d747de15974339cc0b01ddc414583e0
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-09 16:44:32 +0100

    Step 3 Solution

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
