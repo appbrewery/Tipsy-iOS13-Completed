da9380730864e615f886addf0c921284aa75c7d9
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-10 09:30:58 +0100

    Step 6 Solution

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -18,8 +18,13 @@ class CalculatorViewController: UIViewController {
     
     var tip = 0.10
     var numberOfPeople = 2
+    var billTotal = 0.0
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        //Dismiss the keyboard when the user chooses one of the tip values.
+        billTextField.endEditing(true)
+        
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
@@ -33,17 +38,30 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        //Get the stepper value using sender.value, round it down to a whole number then set it as the text in
-        //the splitNumberLabel
         splitNumberLabel.text = String(format: "%.0f", sender.value)
-        
-        //Set the numberOfPeople property as the value of the stepper as a whole number.
         numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print(numberOfPeople)
+        
+        //Get the text the user typed in the billTextField
+        let bill = billTextField.text!
+        
+        //If the text is not an empty String ""
+        if bill != "" {
+            
+            //Turn the bill from a String e.g. "123.50" to an actual String with decimal places.
+            //e.g. 125.50
+            billTotal = Double(bill)!
+            
+            //Multiply the bill by the tip percentage and divide by the number of people to split the bill.
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            //Round the result to 2 decimal places and turn it into a String.
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+        }
     }
+    
     
 }
 
